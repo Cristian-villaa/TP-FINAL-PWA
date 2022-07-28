21c4405a99fd7cd992c5d58a70f07f7888f4b9a7
Cristian-villaa <cristianporras.v@gmail.com>
2022-07-28 12:23:27 -0300

    Parcial Nro.2 6) Implementar la clase IdentityInterface

--- src/basic/models/Usuario.php
+++ src/basic/models/Usuario.php
@@ -15,7 +15,7 @@ use Yii;
  * @property string $accessToken
  * @property string $authKey
  */
-class Usuario extends \yii\db\ActiveRecord
+class Usuario extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
 {
     /**
      * {@inheritdoc}
@@ -53,4 +53,75 @@ class Usuario extends \yii\db\ActiveRecord
             'authKey' => 'Auth Key',
         ];
     }
+
+  /**
+     * {@inheritdoc}
+     */
+    public static function findIdentity($id)
+    {
+        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        foreach (self::$users as $user) {
+            if ($user['accessToken'] === $token) {
+                return new static($user);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        foreach (self::$users as $user) {
+            if (strcasecmp($user['username'], $username) === 0) {
+                return new static($user);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    
+    public function getAuthKey()
+    {
+        return $this->authKey;
+    }
+
+   
+    public function validateAuthKey($authKey)
+    {
+        return $this->authKey === $authKey;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+
 }
